fc463d3bd224bd99c04058c5808c4d135372c7c2
PannaAryan <panna.codebase@gmail.com>
2024-11-21 16:50:38 +0600

    Implementation and documenting using drf_spectacular

--- manage.py
+++ manage.py
@@ -2,15 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from ecommerce.settings import base
+
+from ecommerce import settings
 
 
 def main():
     """Run administrative tasks."""
-    if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.local')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
